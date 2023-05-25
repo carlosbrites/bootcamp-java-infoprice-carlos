147b7baf7a778f7cd2d9327a4b885af044225e51
carlosbrites <carloseduardodbrites@gmail.com>
2023-05-25 14:15:40 -0300

    First Commit

---
+++ src/main/java/co/infoprice/carlos/CarlosApplication.java
@@ -0,0 +1,13 @@
+package co.infoprice.carlos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarlosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarlosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/infoprice/carlos/CarlosApplicationTests.java
@@ -0,0 +1,13 @@
+package co.infoprice.carlos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarlosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
